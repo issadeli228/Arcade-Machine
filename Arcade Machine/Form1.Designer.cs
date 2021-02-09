370b3cd5e10b8db585ac105b48ec3bca9f5ffaaf
issac deline <issadeli228@outlook.com>
2021-02-09 10:10:18 -0500

    test changes

--- Arcade Machine/Form1.Designer.cs
+++ Arcade Machine/Form1.Designer.cs
@@ -111,7 +111,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.ClientSize = new System.Drawing.Size(743, 442);
             this.Controls.Add(this.GoLabel);
             this.Controls.Add(this.CountdownLabel1);
             this.Controls.Add(this.CountdownLabel2);
